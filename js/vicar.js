dcbf3808b86bdf6a450ffde826176ec80757732f
cheriyankm <cheriyankmaman@gmail.com>
2020-09-20 20:07:26 +0530

    Update vicar.js

--- js/vicar.js
+++ js/vicar.js
@@ -1,6 +1,6 @@
 var app = angular.module("myApp", []);
 app.controller("myCtrl", function($scope, $http) {
-    /*$scope.records = [];
+    $scope.records = [];
     $http({
         method: 'GET',
         url: 'https://jsonbin.org/cheriyankmaman/vicars',
@@ -9,23 +9,6 @@ app.controller("myCtrl", function($scope, $http) {
         }
     }).then(function(response) {
         $scope.records = response.data;
-    });*/
-    $scope.records = [];
-    $http.get("https://dl.dropbox.com/s/6w4c7f69uthtrxq/vicar.csv?dl=0")
-        .then(function(response) {
-        $scope.csvRecord = response.data;
-        var lines = $scope.csvRecord.split("\r\n");
-            var result = [];
-            var headers = lines[0].split(",");
-            for (var i = 1; i < lines.length; i++) {
-                var obj = {};
-                var currentline = lines[i].split(",");
-                for (var j = 0; j < headers.length; j++) {
-                    obj[headers[j]] = currentline[j];
-                }
-                result.push(obj);
-            }
-        $scope.records = result;
     });
 });
 
